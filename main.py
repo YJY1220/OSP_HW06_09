f369f61965fd87864904f1c9f4c2229d9f8739ae
YJY1220 <jiyeyu1220@gmail.com>
2022-04-26 21:45:36 +0900

    pass changed

--- main.py
+++ main.py
@@ -4,11 +4,12 @@
 import sys
 import bin_to_hex
 import setcys
+import fibonacci
 
 def print_menu():
-    print("Select menu: 1)bin 2)set 3)fibo 4)exit ?")           
+    print("Select menu: 1)bin 2)set 3)fibo 4)exit ?", end="")           
      
-    inputed_number = int(input("", end=""))
+    inputed_number = int(input(""))
       
     if inputed_number == 1:
        bin_to_hex.binTohex()
@@ -17,7 +18,8 @@ def print_menu():
        setcys.set1()
        print()
     elif inputed_number == 3:
-       print("3")         
+       fibonacci.fibonacci() 
+       print()
     elif inputed_number == 4:
        print("exit the program...")
        exit()
