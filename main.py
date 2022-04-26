5c1119abb7c1719735cba8167f1b2fe2bbc2f540
YJY1220 <jiyeyu1220@gmail.com>
2022-04-26 20:46:47 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+        def print_menu():
+                print("Select menu: 1)bin 2)set 3)fibo 4)exit ?")
+                
+                inputed_number = int(input(" "))
+
+                if inputed_number == 1:
+                
+                elif inputed_number == 2:
+                
+                elif inputed_number == 3:
+                
+                elif inputed_number == 4:
+                    print("exit the program...")
+                    exit()
+                else:                                                                               print("번호를 다시 입력하세요.")
+                                                                                           while True:
+                                                                                               print_menu()
+
