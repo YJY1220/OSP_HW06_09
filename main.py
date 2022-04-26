8b70399df7c322037c50fdc8763fd77a8d8d641b
YJY1220 <jiyeyu1220@gmail.com>
2022-04-26 22:00:37 +0900

    all complete

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 #!/usr/bin/python
 import sys
-import bin_to_hex
-import setcys
-import fibonacci
+#import bin_to_hex
+#import setcys
+#import fibonacci
+import OSP_HW06.bin_to_hex
+import OSP_HW06.setcys
+import OSP_HW06.fibonacci
 
 def print_menu():
     print("Select menu: 1)bin 2)set 3)fibo 4)exit ?", end="")           
@@ -12,13 +15,13 @@ def print_menu():
     inputed_number = int(input(""))
       
     if inputed_number == 1:
-       bin_to_hex.binTohex()
+       OSP_HW06.bin_to_hex.binTohex()
        print()
     elif inputed_number == 2:
-       setcys.set1()
+       OSP_HW06.setcys.set1()
        print()
     elif inputed_number == 3:
-       fibonacci.fibonacci() 
+       OSP_HW06.fibonacci.fibonacci() 
        print()
     elif inputed_number == 4:
        print("exit the program...")
