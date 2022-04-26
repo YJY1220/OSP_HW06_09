ed016939308e8a3311ff616144a7c49f59e7eec1
YJY1220 <jiyeyu1220@gmail.com>
2022-04-26 21:26:24 +0900

    main + no.1 module

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+#2021113750 유지예
+
 #!/usr/bin/python
 import sys
+import bin_to_hex
 def print_menu():
     print("Select menu: 1)bin 2)set 3)fibo 4)exit ?")           
      
     inputed_number = int(input(" "))
       
     if inputed_number == 1:
-       print("1")
-
+       bin_to_hex.binTohex()
     elif inputed_number == 2:
        print("2")
     elif inputed_number == 3:
@@ -16,5 +18,5 @@ def print_menu():
        print("exit the program...")
        exit()
     else:                                                                              print("번호를 다시 입력하세요.")
-if__name__=='__main__':
+if __name__ == "__main__":
     while True:                                                                        print_menu()
