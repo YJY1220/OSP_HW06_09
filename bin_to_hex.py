c856f12e25b9eb5dff8e64347a51b9781c371e83
송혜경 <sosschs9@naver.com>
2022-04-26 20:31:50 +0900

    add file

---
+++ bin_to_hex.py
@@ -0,0 +1,9 @@
+# bin_to_hex_conversion
+# 2021111849 Song Hyegyeong
+
+if __name__ == '__main__':
+     num_2 = input("input bin number: ")
+     num_10 = int(num_2, 2)
+     num_16 = hex(num_10i)
+
+     print("hexa number: " + num_16)
